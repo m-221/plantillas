ed7d755f1696f630058746703ef08c3b191529f2
m-221 <24308060610066@cetis61.edu.mx>
2025-10-13 07:55:04 -0600

    mm

--- app.py
+++ app.py
@@ -1,28 +1,24 @@
 from flask import Flask,render_template
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('uno.html')
-    
 
 @app.route('/')
 def index0():
     return render_template('inicio.html')
 
-@app.route('/')
+@app.route('/animales')
 def index2():
     return render_template('animales.html')
 
-@app.route('/')
+@app.route('/vehiculos')
 def index3():
     return render_template('vehiculos.html')
 
-@app.route('/')
+@app.route('/maravillas')
 def index4():
     return render_template('maravillas.html')
 
-@app.route('/')
+@app.route('/acercas')
 def index5():
     return render_template('mas.html')
 
